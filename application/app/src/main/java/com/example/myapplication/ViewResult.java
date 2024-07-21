b37a74fb88076ca6819d1217b170d1d27c75ec6f
Megalos vigneswaran <t896242@gmail.com>
2024-07-21 21:17:09 +0530

    ViewResult.java updated

--- application/app/src/main/java/com/example/myapplication/ViewResult.java
+++ application/app/src/main/java/com/example/myapplication/ViewResult.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,27 @@ public class ViewResult extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private TextView CTV(String text, int textS, int width, boolean pad) {
+        TextView textView = new TextView(this);
+        textView.setText(text);
+        TableRow.LayoutParams params = new TableRow.LayoutParams(
+                width,
+                TableRow.LayoutParams.WRAP_CONTENT
+        );
+        if(pad){
+            params.setMargins(0, 10, 9, 0);
+        }else{
+            params.setMargins(0, 10, 0, 0);
+        }
+        textView.setLayoutParams(params);
+        textView.setGravity(Gravity.CENTER);
+        textView.setTextSize(textS);
+        textView.setTextColor(Color.WHITE);
+        textView.setBackgroundColor(Color.parseColor("#BF32BC32"));
+        textView.setTypeface(null, android.graphics.Typeface.BOLD);
+
+        return textView;
     }
 }
